5fa06ea3530134d87d0f8a32ea8faf5741845492
Brandon S. Kane <me@brandonskane.com>
2017-02-21 22:12:23 -0500

    Swift 3 Update

---
+++ GeoLocation/GeoLocation.h
@@ -0,0 +1,19 @@
+//
+//  GeoLocation.h
+//  GeoLocation
+//
+//  Created by Brandon S. Kane on 2/21/17.
+//  Copyright © 2017 Brandon S. Kane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoLocation.
+FOUNDATION_EXPORT double GeoLocationVersionNumber;
+
+//! Project version string for GeoLocation.
+FOUNDATION_EXPORT const unsigned char GeoLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoLocation/PublicHeader.h>
+
+
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 A Swift implementation of http://janmatuschek.de/LatitudeLongitudeBoundingCoordinates#Java
+
+## Carthage
+`github brandonskane/GeoLocation_Swift`
